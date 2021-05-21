5b80e9daf40388745390787ed7ee3248aabc8f63
Larissa Cunha <picpay@PPM-VIX-2094.local>
2021-05-21 12:00:09 -0300

    colocando o conteudo de componente dentro do portal

--- src/app/cdk-portal-overview-example/cdk-portal-overview-example.component.ts
+++ src/app/cdk-portal-overview-example/cdk-portal-overview-example.component.ts
@@ -1,12 +1,10 @@
-import {ComponentPortal, DomPortal, Portal, TemplatePortal} from '@angular/cdk/portal';
+import {ComponentPortal, Portal} from '@angular/cdk/portal';
 import {
   AfterViewInit,
   Component,
-  TemplateRef,
-  ViewChild,
   ViewContainerRef,
-  ElementRef
 } from '@angular/core';
+import { HeroesComponent } from '../heroes/heroes.component';
 
 /**
  * @title Portal overview
@@ -17,30 +15,13 @@ import {
   styleUrls: ['cdk-portal-overview-example.component.css'],
 })
 export class CdkPortalOverviewExampleComponent implements AfterViewInit {
-  @ViewChild('templatePortalContent')
-  templatePortalContent!: TemplateRef<unknown>;
-  @ViewChild('domPortalContent')
-  domPortalContent!: ElementRef<HTMLElement>;
 
   selectedPortal!: Portal<any>;
-  componentPortal!: ComponentPortal<ComponentPortalExample>;
-  templatePortal!: TemplatePortal<any>;
-  domPortal!: DomPortal<any>;
+  componentPortal!: ComponentPortal<HeroesComponent>;
 
   constructor(private _viewContainerRef: ViewContainerRef) { }
 
   ngAfterViewInit() {
-    this.componentPortal = new ComponentPortal(ComponentPortalExample);
-    this.templatePortal = new TemplatePortal(
-      this.templatePortalContent,
-      this._viewContainerRef
-    );
-    this.domPortal = new DomPortal(this.domPortalContent);
+    this.componentPortal = new ComponentPortal(HeroesComponent);
   }
 }
-
-@Component({
-  selector: 'component-portal-example',
-  template: 'Hello, this is a component portal'
-})
-export class ComponentPortalExample { }
